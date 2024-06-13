7d8bb292e48cf3ba3e455f1fe74bed4f460a6850
ahmedaisar <ahmed.aisar@gmail.com>
2024-06-14 03:10:10 +0500

    edge#39

--- api/maldives.js
+++ api/maldives.js
@@ -10,12 +10,12 @@ async function getBrowser() {
   // Adjust browser launch configuration
   return puppeteer.launch({
     args: [
-      ...chromium.args,
-      '--hide-scrollbars',
-      '--disable-web-security',
+      //...chromium.args,
+      //'--hide-scrollbars',
+      //'--disable-web-security',
       '--no-sandbox',
       '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage'
+      //'--disable-dev-shm-usage'
     ],
     executablePath: executablePath,
     headless: chromium.headless,
@@ -33,10 +33,6 @@ module.exports = async (req, res) => {
   try {
     browser = await getBrowser();
     const page = await browser.newPage();
-
-    await page.goto('https://hotelscan.com/', {
-      waitUntil: "networkidle2",
-    });
 
     await page.goto('https://hotelscan.com/combiner?pos=zz&locale=en&checkin=2024-07-23&checkout=2024-07-28&rooms=2&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0', {
       waitUntil: "networkidle2",
