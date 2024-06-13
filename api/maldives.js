2a1cbf47da76ecc7048835337e1d0de2a7d89165
ahmedaisar <ahmed.aisar@gmail.com>
2024-06-14 03:39:21 +0500

    edge#47

--- api/maldives.js
+++ api/maldives.js
@@ -24,7 +24,7 @@ module.exports = async (req, res) => {
     });
 
     const page = await browser.newPage();
-    await page.goto('https://jsonplaceholder.typicode.com/posts', { waitUntil: 'networkidle2', timeout: 60000 });
+    await page.goto('https://hotelscan.com/combiner?pos=zz&locale=en&checkin=2024-07-23&checkout=2024-07-28&rooms=2&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0z', { waitUntil: 'networkidle2', timeout: 20000 });
     const content = await page.content();
 
     res.status(200).send(content);
