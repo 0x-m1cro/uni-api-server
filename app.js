d5dcc5f9a579d74ba212811618e305183b251569
ahmedaisar <ahmed.aisar@gmail.com>
2024-06-13 23:28:43 +0500

    express#14

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const chromium = require('@sparticuz/chromium-min');
 const puppeteer = require('puppeteer-core');
-const express = require('express')
-const app = express()
-const port = 8000
+// const express = require('express')
+// const app = express()
+// const port = 8000
 let _page;
 
 async function getBrowser() {
@@ -27,10 +27,35 @@ async function getPage() {
   return _page;
 }
  
-app.get('/', (req, res) => { res.send('Welcome') })
+// app.get('/', (req, res) => { res.send('Welcome') })
 
-app.get('/api/maldives', async (req, res) => {
+// app.get('/api/maldives', async (req, res) => {
   
+//   try {
+//     const page = await getPage();
+//     await page.goto('https://hotelscan.com/combiner?pos=zz&locale=en&checkin=2024-07-23&checkout=2024-07-28&rooms=2&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0',
+//         {
+//           waitUntil: "networkidle2",
+//           timeout: 0
+//         });
+
+//     let body = await page.waitForSelector('body');
+//     let json = await body?.evaluate(el => JSON.parse(el.textContent));
+//     await browser.close();   
+//     res.status(200).json(json);       
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   } finally {
+//     if (browser !== null) {
+//       await browser.close();
+//     }
+//   }
+// })
+
+// app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+module.exports = async (req, res) => {
+ 
   try {
     const page = await getPage();
     await page.goto('https://hotelscan.com/combiner?pos=zz&locale=en&checkin=2024-07-23&checkout=2024-07-28&rooms=2&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0',
@@ -50,8 +75,4 @@ app.get('/api/maldives', async (req, res) => {
       await browser.close();
     }
   }
-})
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
-
-module.exports = app
+};
