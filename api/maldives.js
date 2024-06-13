6d7be971f88619d9a67a073ec258dcc9fa49f1cf
ahmedaisar <ahmed.aisar@gmail.com>
2024-06-14 02:41:17 +0500

    edge#29

--- api/maldives.js
+++ api/maldives.js
@@ -25,6 +25,9 @@ async function getBrowser() {
 
 module.exports = async (req, res) => {
   let browser;
+  let query = req.query;
+  // const { adults, child, checkin, checkout } = query;
+  // const c = child ? 'f' : ''
 
   try {
     browser = await getBrowser();
@@ -33,14 +36,16 @@ module.exports = async (req, res) => {
     // Set a generous timeout and wait for network to be idle
     await page.goto('https://hotelscan.com/combiner?pos=zz&locale=en&checkin=2024-07-23&checkout=2024-07-28&rooms=2&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0', {
       waitUntil: "networkidle2",
+       timeout: "0"
     });
     await page.goto('https://hotelscan.com/combiner?pos=zz&locale=en&checkin=2024-07-23&checkout=2024-07-28&rooms=2&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0', {
       waitUntil: "networkidle2",
+      timeout: "0"
     });
 
     let body = await page.waitForSelector('body'); // Wait for body selector to ensure the page has loaded
     let json = await body?.evaluate(el => el.textContent);
-
+    await browser.close();   
     res.status(200).json(json);
   } catch (error) {
     console.error('Error occurred:', error);  
