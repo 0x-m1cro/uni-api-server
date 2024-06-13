791491562f358c705da3ac32f0fb5cc2eb91f7cd
ahmedaisar <ahmed.aisar@gmail.com>
2024-06-13 22:27:55 +0500

    express#2

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ async function getBrowser() {
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8000
 
 app.get('/', (req, res) => { res.send('Welcome') })
 
@@ -43,7 +43,7 @@ app.get('/api/maldives', async (req, res) => {
   }
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
 
 module.exports = app;
  
