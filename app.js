21baf2cd98276aa39518d82dfd882d63983adedd
ahmedaisar <ahmed.aisar@gmail.com>
2024-06-13 22:47:39 +0500

    edge

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const chromium = require('@sparticuz/chromium-min');
 const puppeteer = require('puppeteer-core');
-const express = require('express')
-const app = express()
-const port = 8000
+// const express = require('express')
+// const app = express()
+// const port = 8000
 
 async function getBrowser() {
   return puppeteer.launch({
@@ -17,33 +17,53 @@ async function getBrowser() {
 }
 
 
-app.get('/', (req, res) => { res.send('Welcome') })
+// app.get('/', (req, res) => { res.send('Welcome') })
 
-app.get('/api/maldives', async (req, res) => {
+// app.get('/api/maldives', async (req, res) => {
   
-  try {
-    let browser = await getBrowser();
-    let page = await browser.newPage();
-    await page.goto('https://hotelscan.com/combiner?pos=zz&locale=en&checkin=2024-07-23&checkout=2024-07-28&rooms=2&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0',
-        {
-          waitUntil: "networkidle2",
-          timeout: 0
-        });
+//   try {
+//     let browser = await getBrowser();
+//     let page = await browser.newPage();
+//     await page.goto('https://hotelscan.com/combiner?pos=zz&locale=en&checkin=2024-07-23&checkout=2024-07-28&rooms=2&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0',
+//         {
+//           waitUntil: "networkidle2",
+//           timeout: 0
+//         });
 
-    let body = await page.waitForSelector('body');
-    let json = await body?.evaluate(el => JSON.parse(el.textContent));
-    await browser.close();   
-    res.status(200).json(json);       
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  } finally {
-    if (browser !== null) {
-      await browser.close();
+//     let body = await page.waitForSelector('body');
+//     let json = await body?.evaluate(el => JSON.parse(el.textContent));
+//     await browser.close();   
+//     res.status(200).json(json);       
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   } finally {
+//     if (browser !== null) {
+//       await browser.close();
+//     }
+//   }
+// })
+
+// app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+module.exports = async (req, res) => {
+    const browser = await getBrowser();
+    const page = await browser.newPage();
+    try {
+      await page.goto('https://hotelscan.com/combiner?pos=zz&locale=en&checkin=2024-07-23&checkout=2024-07-28&rooms=2&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0',
+          {
+            waitUntil: "networkidle2",
+            timeout: 0
+          });
+  
+      let body = await page.waitForSelector('body');
+      let json = await body?.evaluate(el => JSON.parse(el.textContent));
+      await browser.close();   
+      res.status(200).json(json);       
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    } finally {
+      if (browser !== null) {
+        await browser.close();
+      }
     }
   }
-})
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
-
-module.exports = app;
- 
