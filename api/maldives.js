f124593dc6e5528f5c007fea72f98ee744bfc94f
ahmedaisar <ahmed.aisar@gmail.com>
2024-06-14 02:52:12 +0500

    edge#34

--- api/maldives.js
+++ api/maldives.js
@@ -39,7 +39,6 @@ module.exports = async (req, res) => {
       waitUntil: "networkidle2",
       timeout: "0"
     });
-    await page.close();
 
     await page.goto('https://hotelscan.com/combiner?pos=zz&locale=en&checkin=2024-07-23&checkout=2024-07-28&rooms=2&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0', {
       waitUntil: "networkidle2",
