be84dd25c0f6659799e282a7f733314b7ae45952
ahmedaisar <ahmed.aisar@gmail.com>
2024-06-14 03:28:20 +0500

    edge#42

--- api/maldives.js
+++ api/maldives.js
@@ -1,48 +1,35 @@
 const puppeteer = require('puppeteer-core');
 const chromium = require('@sparticuz/chromium-min');
 
-async function getBrowser() {
-  // Ensure the executablePath URL is correct and points to a compatible version
-  const executablePath = await chromium.executablePath(
-    `https://github.com/Sparticuz/chromium/releases/download/v123.0.1/chromium-v123.0.1-pack.tar`
-  );
-
-  // Adjust browser launch configuration
-  return puppeteer.launch({
-    args: [
-      ...chromium.args,
-      //'--hide-scrollbars',
-      //'--disable-web-security',
-      // '--no-sandbox',
-      // '--disable-setuid-sandbox',
-      //'--disable-dev-shm-usage'
-    ],
-    executablePath: executablePath,
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-    dumpio: true // Enables more detailed logging
-  });
-}
-
 module.exports = async (req, res) => {
   let browser;
-  let query = req.query;
-  // const { adults, child, checkin, checkout } = query;
-  // const c = child ? 'f' : ''
 
   try {
-    browser = await getBrowser();
-    const page = await browser.newPage();
+    const executablePath = await chromium.executablePath(
+      `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+    );
 
-    await page.goto('https://hotelscan.com/combiner?pos=zz&locale=en&checkin=2024-07-23&checkout=2024-07-28&rooms=2&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0', {
-      waitUntil: "networkidle2",
+    browser = await puppeteer.launch({
+      args: [
+        ...chromium.args,
+        '--hide-scrollbars',
+        '--disable-web-security',
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+      ],
+      executablePath: executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+      dumpio: true
     });
 
-    let body = await page.waitForSelector('body'); // Wait for body selector to ensure the page has loaded
-    let json = await body?.evaluate(el => el.textContent);
-    res.status(200).json(json);
+    const page = await browser.newPage();
+    await page.goto('https://jsonplaceholder.typicode.com/posts', { waitUntil: 'networkidle2', timeout: 60000 });
+    const content = await page.content();
+
+    res.status(200).send(content);
   } catch (error) {
-    console.error('Error occurred:', error);  
+    console.error('Error occurred:', error.message);
     res.status(500).json({ error: error.message });
   } finally {
     if (browser) {
