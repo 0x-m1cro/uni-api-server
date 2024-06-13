6e8753041df260777f17e925e015cfaef1b1658a
ahmedaisar <ahmed.aisar@gmail.com>
2024-06-14 02:49:02 +0500

    edge#32

--- api/maldives.js
+++ api/maldives.js
@@ -37,11 +37,11 @@ module.exports = async (req, res) => {
     // Set a generous timeout and wait for network to be idle
     await page.goto('https://hotelscan.com/', {
       waitUntil: "networkidle2",
-      timeout: "2000"
+      timeout: "0"
     });
     await page.goto('https://hotelscan.com/combiner?pos=zz&locale=en&checkin=2024-07-23&checkout=2024-07-28&rooms=2&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0', {
       waitUntil: "networkidle2",
-      timeout: "2000"
+      timeout: "0"
     });
 
     let body = await page.waitForSelector('body'); // Wait for body selector to ensure the page has loaded
