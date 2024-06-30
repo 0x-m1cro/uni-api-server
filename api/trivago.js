4e9333b51693ef3d9f3bda7d020a558da51c8cdb
ahmedaisar <ahmed.aisar@gmail.com>
2024-06-30 16:34:42 +0500

    fix trivago #76

--- api/trivago.js
+++ api/trivago.js
@@ -24,6 +24,7 @@ module.exports = async (req, res) => {
       dumpio: true
     });
       const page = await browser.newPage();
+      await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36')
       
       await page.setRequestInterception(true)
 
