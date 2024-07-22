eb199afabc0731533d0417f62a1f95c56e271186
ahmedaisar <ahmed.aisar@gmail.com>
2024-07-22 17:24:33 +0500

    trivago updated#13

--- api/trivago.js
+++ api/trivago.js
@@ -23,7 +23,9 @@ module.exports = async (req, res) => {
     });
       const page = await browser.newPage();
 
-      await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36')
+      const ua = await page.evaluate('navigator.userAgent');
+
+      await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1')
       
       await page.setRequestInterception(true)
 
