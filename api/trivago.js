0ec178601b355282aea1fc7e84e9696941ef0450
ahmedaisar <ahmed.aisar@gmail.com>
2024-07-22 16:46:56 +0500

    trivago updated#2

--- api/trivago.js
+++ api/trivago.js
@@ -14,6 +14,7 @@ module.exports = async (req, res) => {
     
     browser = await puppeteer.launch({
       args: [
+        ...chromium.args,
         '--hide-scrollbars', 
         '--disable-web-security',
       ],
